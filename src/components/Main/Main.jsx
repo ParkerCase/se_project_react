c944651d44eda784f1d5c739f5c613438623af0d
ParkerCase <ParkerE.Case@gmail.com>
2024-07-18 20:41:05 -0700

    through video 11

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,34 @@
-import WeatherCard from "../WeatherCard/WeatherCard";
 import "./Main.css";
 
-function Main() {
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+
+function Main({ weatherData, handleCardClick }) {
   return (
-    <Main>
+    <main>
       <WeatherCard />;
       <section className="cards">
         <p className="cards__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
-        {/* Add Cards here */}
+        <ul className="cards__list">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={handleCardClick}
+                />
+              );
+            })}
+        </ul>
       </section>
-    </Main>
+    </main>
   );
 }
 
